ea3919eb1c60545dc981df648ab316d083d6bece
Veerpratapsi <veerpratap1127@gmail.com>
2025-02-24 22:56:02 +0530

    first commit

---
+++ camp.php
@@ -0,0 +1,57 @@
+<?php	
+	
+	include ('include/header.php'); 
+
+?>
+
+<style>
+	.size{
+		min-height: 0px;
+		padding: 60px 0 40px 0;
+		
+	}
+	.loader{
+		display:none;
+		width:69px;
+		height:89px;
+		position:absolute;
+		top:25%;
+		left:50%;
+		padding:2px;
+		z-index: 1;
+	}
+	.loader .fa{
+		color: #e74c3c;
+		font-size: 52px !important;
+	}
+	.form-group{
+		text-align: left;
+	}
+	h1{
+		color: white;
+	}
+	h3{
+		color: #e74c3c;
+		text-align: center;
+	}
+	.red-bar{
+		width: 25%;
+	}
+	span{
+		display: block;
+	}
+	.name{
+		color: #e74c3c;
+		font-size: 22px;
+		font-weight: 700;
+	}
+</style>
+
+    <div class="container-fluid red-background size">
+		<div class="row">
+			<div class="col-md-6 offset-md-3">
+				<h1 class="text-center">Camps</h1>
+				<hr class="white-bar">
+			</div>
+		</div>
+	</div>
