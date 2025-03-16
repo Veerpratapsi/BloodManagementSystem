7a22e9a3af1b74e060c3ff7e69bcc3077dec194b
Veerpratapsi <veerpratap1127@gmail.com>
2025-03-16 20:10:28 +0530

    database connected

---
+++ include/connection.php
@@ -0,0 +1,23 @@
+<?php
+
+$serverName = "VEERPRATAP\SQLEXPRESS";
+$database="Blood_Management_System";
+$uid="";
+$pass="";
+
+$Connection=[
+    "Database" => $database,
+    "UID" =>$uid,
+    "PWD" =>$pass
+];
+
+$conn= sqlsrv_connect($serverName,$Connection);
+
+if (!$conn) 
+    die(print_r(sqlsrv_errors(), true));
+ else 
+    echo "Connection successful!";
+?>
+
+
+
